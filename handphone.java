c46b219df853d3b0a13687d06584f886756a7a29
icedd777 <dumatubunrisat@gmail.com>
2024-12-27 17:03:37 +0700

    Add files via upload

---
+++ handphone.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package handphone;
+
+/**
+ *
+ * @author Risat Dumatubun
+ */
+public class handphone {
+     void hidupkan(){
+        System.out.println("hp udah lama dinyalakan");
+    }
+    void lakukanPanggilan(){
+        System.out.println("auww auww sedang melalukan PANGGILAN");
+    }
+    void kirimSMS(){
+        System.out.println("KRIK KRIK WA UDAH DIKIRIMMMM ");
+    }
+    void matikan(){
+        System.out.println("HAPEnya UDAH TURNED OFF");
+    }
+    
+}
+    
+
---
+++ handphoneBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package handphone;
+
+/**
+ *
+ * @author Risat Dumatubun
+ */
+public class handphoneBeraksi {
+    public static void main(String[] args) {
+        handphone hp = new handphone();
+        hp.hidupkan();
+        hp.lakukanPanggilan();
+        hp.kirimSMS();
+        hp.matikan();
+    }
+}
+   
